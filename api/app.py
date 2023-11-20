701d86fd28e87c2b540270ddaea8e557e5517623
Jonatas E S Silva <jonatassilvaemanuel@gmail.com>
2023-11-19 22:17:19 -0300

    feat: get all tags

--- api/app.py
+++ api/app.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 
-from api.routes import article, auth, user
+from api.routes import article, auth, user, tags
 
 app = FastAPI()
 
 app.include_router(article.router)
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(tags.router)
 
 
 @app.get('/health-check')
